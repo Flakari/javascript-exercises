30a2d17c47830b3d86f3c922356694442acbca5d
Nate Dimock <natedimock@yahoo.com>
2018-01-13 12:05:14 -0600

    Finished and tested remaining pig_latin exercises

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -9,14 +9,40 @@ function translate(phrase) {
 
 		if (vowelExp.test(word.charAt(0))) {
 			tempPhrase.push('ay');
+			tempPhrase = tempPhrase.join('');
+			newPhrase.push(tempPhrase);
+			tempPhrase = [];
 		}
 
 		if (!vowelExp.test(word.charAt(0))) {
-			word.split('');
+			let findVowel = 0;
+			let slicePoint = 0;
+			let i = 0;
+			while (findVowel < 1) {
+				if (vowelExp.test(tempPhrase[0].charAt(i))) {
+					findVowel += 1;
+					slicePoint = i;
+				} else {
+					i++;
+				}
+			}
+
+			if(tempPhrase[0].charAt(slicePoint) == 'u') {
+				if (tempPhrase[0].charAt(slicePoint - 1) == 'q') {
+					slicePoint += 1;
+				}
+			}
+			tempPhrase.push(tempPhrase[0].slice(slicePoint));
+			tempPhrase.push(tempPhrase[0].slice(0, slicePoint));
+			tempPhrase.push('ay');
+			tempPhrase.shift();
+			tempPhrase = tempPhrase.join('');
+			newPhrase.push(tempPhrase);
+			tempPhrase = [];
 		}
 	});
-	tempPhrase = tempPhrase.join('');
-	return tempPhrase;
+	newPhrase = newPhrase.join(' ');
+	return newPhrase;
 }
 
 
