25459c39f6c8abafe8f4681a9fa0447c35a98a72
Nate Dimock <natedimock@yahoo.com>
2018-01-12 23:24:12 -0600

    Added and tested first of pigLatin solution

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,22 @@
-function translate() {
-	// body...
+function translate(phrase) {
+	let newPhrase = [];
+	let tempPhrase = [];
+	let vowelExp = /[aeiou]/;
+	phrase = phrase.split(' ');
+	
+	phrase.forEach(word => {
+		tempPhrase.push(word);
+
+		if (vowelExp.test(word.charAt(0))) {
+			tempPhrase.push('ay');
+		}
+
+		if (!vowelExp.test(word.charAt(0))) {
+			word.split('');
+		}
+	});
+	tempPhrase = tempPhrase.join('');
+	return tempPhrase;
 }
 
 
